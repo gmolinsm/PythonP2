40250d70cfcd6726cab0bc9a69dee4fc4defff0b
Guillermo <guimolins@hotmail.com>
2018-09-25 11:20:59 +0200

    Second set of exercises: Conditionals Exceptions Loops Power of two numbers with additions Magic Maze

--- PowerSum/main.py
+++ PowerSum/main.py
@@ -3,10 +3,15 @@ exponent = int(input("Please give the exponent: "))
 
 currentBase = base
 
-for x in range(0, exponent-1):
-    multiple = 0
-    for y in range(0, base):
-        multiple += currentBase
-    currentBase = multiple
-print(base, "to the power of", exponent, "equals", currentBase)
+if exponent == 0:
+    print(base, "to the power of", exponent, "equals 1")
+    exit()
+else:
+    for x in range(0, exponent - 1):
+        multiple = 0
+        for y in range(0, base):
+            multiple += currentBase
+        currentBase = multiple
+    print(base, "to the power of", exponent, "equals", currentBase)
+
 
